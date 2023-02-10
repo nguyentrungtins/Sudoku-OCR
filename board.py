72b94ff526452774fb2cee849f60070d2e27af8a
nguyentrungtin <nguyentrungtin.tech@gmail.com>
2023-02-10 23:20:26 +0700

    rollback to image requiremets

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-path = r'./sudoku.jpg'
+path = r'./sudoku2.jpg'
 def perspective_transform(image, corners):
     def order_corner_points(corners):
         # Separate corners into individual points
